ef24537ca4fc8770f882f02464f2a3d8913f6802
ESC18 <ponderosaprintingus@gmail.com>
2023-07-14 16:06:56 -0700

    Finish project - All tests pass, logic works properly, UI is fully functional and nice looking.

--- PierBakery.Solution/PierBakeryMain/Program.cs
+++ PierBakery.Solution/PierBakeryMain/Program.cs
@@ -5,9 +5,43 @@ namespace UI
 {
     class Program
     {
-        static void Main() 
+        static void Main()
         {
-            
+            Console.WriteLine(@"
+███████████   ███                                         ██            ███████████            █████                                   
+░░███░░░░░███ ░░░                                         ███           ░░███░░░░░███          ░░███                                    
+ ░███    ░███ ████   ██████  ████████  ████████   ██████ ░░░   █████     ░███    ░███  ██████   ░███ █████  ██████  ████████  █████ ████
+ ░██████████ ░░███  ███░░███░░███░░███░░███░░███ ███░░███     ███░░      ░██████████  ░░░░░███  ░███░░███  ███░░███░░███░░███░░███ ░███ 
+ ░███░░░░░░   ░███ ░███████  ░███ ░░░  ░███ ░░░ ░███████     ░░█████     ░███░░░░░███  ███████  ░██████░  ░███████  ░███ ░░░  ░███ ░███ 
+ ░███         ░███ ░███░░░   ░███      ░███     ░███░░░       ░░░░███    ░███    ░███ ███░░███  ░███░░███ ░███░░░   ░███      ░███ ░███ 
+ █████        █████░░██████  █████     █████    ░░██████      ██████     ███████████ ░░████████ ████ █████░░██████  █████     ░░███████ 
+░░░░░        ░░░░░  ░░░░░░  ░░░░░     ░░░░░      ░░░░░░      ░░░░░░     ░░░░░░░░░░░   ░░░░░░░░ ░░░░ ░░░░░  ░░░░░░  ░░░░░       ░░░░░███ 
+                                                                                                                               ███ ░███ 
+                                                                                                                              ░░██████  
+                                                                                                                               ░░░░░░   
+            ");
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Please enter how many loaves of bread you want: They are $5 each and buy 2 get 1 free!");
+            Console.WriteLine("Enter a number below:");
+            string breadInput = Console.ReadLine();
+
+            Console.WriteLine("Please enter how many pastries you want: They are $2 each and buy 3 get 1 free!");
+            Console.WriteLine("Enter a number below:");
+            string pastryInput = Console.ReadLine();
+
+            int numBread = int.Parse(breadInput);
+            int numPastry = int.Parse(pastryInput);
+
+            BreadPrice breadCalculation = new BreadPrice();
+            int breadCost = breadCalculation.BreadOutput(numBread);
+
+            PastryPrice pastryCalculation = new PastryPrice();
+            int pastryCost = pastryCalculation.PastryOutput(numPastry);
+            int totalCost = pastryCost + breadCost;
+
+            Console.WriteLine($"Total cost for {numBread} loaves of bread: ${breadCost}");
+            Console.WriteLine($"Total cost for {numPastry} pastries: ${pastryCost}");
+            Console.WriteLine($"Total cost for both: ${totalCost}");
         }
     }
 }
---
+++ PierBakery.Solution/README.md
@@ -0,0 +1,34 @@
+# Pierre's Bakery
+
+By Shawn Cartwright
+
+## Technologies Used
+
+- C#
+- .NET Core
+- NUnit (for testing)
+- VSCode C# add on
+
+## Description
+
+Pierre's Bakery is a C# console application that allows users to place an order at Pierre's Bakery. The application calculates the total cost of the order based on the quantity of loaves of bread and pastries the user wants. Pierre's Bakery offers special deals: buy 2 loaves of bread and get 1 free, and buy 3 pastries and get 1 free.
+
+## Setup and Installation
+
+1. First download the repository to your device.
+2. Proceed to navigate to the highest level of your directory.
+3. Open PierBakery in VSCode through your terminal.
+4. Create a .gitignore file for the obj and bin folders which will be auto generated. 
+5. Build the project to restore the NuGet packages by going to the PierBakeryMain directory and 
+    typing _dotnet build_ in the terminal.
+6. Run the program to start the application by entering _dotnet run_.
+7. Follow the on-screen prompts to enter the quantity of bread and pastries desired.
+8. The application will display the total cost of the order.
+
+## Known Issues
+- Does not work with non-integer characters.
+- For best results in terminal: Make terminal Full Screen!
+
+## Licensing
+
+This project is licensed under the MIT License. (c) 2023 Elijah S. Cartwright
