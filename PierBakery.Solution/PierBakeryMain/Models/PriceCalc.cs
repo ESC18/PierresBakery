bde7a0f2a1788a5ea8ab43adb9a298cac92aa4f7
ESC18 <ponderosaprintingus@gmail.com>
2023-07-14 13:29:24 -0700

    First test passed - now outputs the proper value if the user number is divisible by 3

--- PierBakery.Solution/PierBakeryMain/Models/PriceCalc.cs
+++ PierBakery.Solution/PierBakeryMain/Models/PriceCalc.cs
@@ -5,7 +5,18 @@ namespace PriceCalc.Models
     public class BreadPrice 
     {
         public int UserNumBread;
+         public int BreadOutput(int userNumBread) 
+        {
+            if (userNumBread <= 0)
+            {
+                return 0;
+            }
 
+            int freeLoaves = userNumBread / 3;
+            int breadTotal = (userNumBread - freeLoaves) * 5;
+
+            return breadTotal;
+        }
     }
 }
 
