87768f1a6568b369a5847bb15a1f440ceac7c0ef
ESC18 <ponderosaprintingus@gmail.com>
2023-07-14 13:55:02 -0700

    Second test passed - business logic can handle a value of 0

--- PierBakery.Solution/PierBakery.Tests/ModelTests/PriceCalc.Tests.cs
+++ PierBakery.Solution/PierBakery.Tests/ModelTests/PriceCalc.Tests.cs
@@ -15,5 +15,15 @@ namespace PriceCalc.Tests.ModelTests
             int expectedPrice = (userNumBread / 3) * 10;
             Assert.AreEqual(expectedPrice, result);
         }
+        
+        [TestMethod]
+        public void BreadOutput_ReturnsCorrectPrice_ForLessThanOrEqualTo0()
+        {
+            int userNumBread = 0; // Less than or equal to 0
+            BreadPrice breadPrice = new BreadPrice();
+            int result = breadPrice.BreadOutput(userNumBread);
+            int expectedPrice = 0;
+            Assert.AreEqual(expectedPrice, result);
+        }
     }
 }
